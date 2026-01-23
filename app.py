64b01b7e93777aa93b1bca36922b737e158eb907
ksy2604 <ksy2604@hanyang.ac.kr>
2026-01-23 10:36:08 +0900

    Fix: 다섯번째 앱으로 수정

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.title("🚗 GM SW Intern Streamlit Demo 4")
+st.title("🚗 GM SW Intern Streamlit Demo 5")
 
-st.write("안녕하세요! Streamlit으로 만든 네 번째 앱입니다.")
+st.write("안녕하세요! Streamlit으로 만든 다섯 번째 앱입니다.")
 
 name = st.text_input("이름을 입력하세요")
 
